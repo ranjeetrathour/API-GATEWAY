5c42b48d70479e75bf8ba53fa9910e88b69c7b7e
Ranjeet Rathour <ranjeet.rathour@xcelore.com>
2025-01-31 14:56:03 +0530

    add ip wise block filter using bucket4j Library

---
+++ api-gateway/src/main/java/com/example/IpWiseRateLimiter.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@Component
+public class IpWiseRateLimiter extends AbstractGatewayFilterFactory<IpWiseRateLimiter.Config> {
+    private final static int REQUEST_LIMIT = 5;
+    private final static Duration TIME_WINDOW = Duration.ofMinutes(5);
+
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    public IpWiseRateLimiter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return new GatewayFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                String ip = exchange.getRequest().getLocalAddress().getAddress().toString().substring(1, exchange.getRequest().getLocalAddress().getAddress().toString().length() - 1);
+                Bucket bucket = cache.computeIfAbsent(ip, k -> createBucket());
+                if (bucket.tryConsume(1)) {
+                    return chain.filter(exchange);
+                } else if (bucket.getAvailableTokens() < 1) {
+                    exchange.getResponse().setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
+                    return exchange.getResponse().setComplete();
+                } else {
+                    return Mono.empty() ;
+                }
+            }
+        };
+    }
+
+    private Bucket createBucket() {
+        return Bucket4j.builder()
+                .addLimit(Bandwidth.classic(REQUEST_LIMIT, Refill.greedy(REQUEST_LIMIT, TIME_WINDOW)))
+                .build();
+    }
+
+    private String extractIpFromRequest(ServerHttpRequest request) {
+        return request.getLocalAddress().getAddress().getHostAddress();
+    }
+
+
+    public static class Config {
+        // Add your configuration properties for rate-limiting, if needed
+    }
+
+}
